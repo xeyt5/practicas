04a5583f785d01ba588b723593dbb4a460728f8f
Xeyt <cbwyuren500@gmail.com>
2026-01-06 15:46:07 -0600

    programa 26 se agrego el changeOrder in list

--- 26_First_Half_of_Array.py
+++ 26_First_Half_of_Array.py
@@ -7,5 +7,16 @@ class Solution:
         reversed_half = nums[half-1::-1] + nums[half:]
         return reversed_half
     
+    def changeOrder(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mitad = n // 2
+        nueva_lista = nums[mitad:] + nums[:mitad]
+        return nueva_lista
+ 
+
 example = Solution()
-print(example.reverseFirstHalf([1, 2, 3, 4, 5, 6]))  
+print(example.reverseFirstHalf([1, 2, 3, 4, 5, 6]))
+
+
+example2 = Solution()
+print(example.changeOrder([1, 2, 3, 4, 5, 6]))
