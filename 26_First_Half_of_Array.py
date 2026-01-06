d41fb3985ec3767b9e6a4b5cc10de847eeb19e80
Xeyt <cbwyuren500@gmail.com>
2026-01-06 15:52:47 -0600

    programa 26 se agrego el reverseKGroup

--- 26_First_Half_of_Array.py
+++ 26_First_Half_of_Array.py
@@ -12,7 +12,15 @@ class Solution:
         mitad = n // 2
         nueva_lista = nums[mitad:] + nums[:mitad]
         return nueva_lista
- 
+
+    def reverseKGroup(self, nums: List[int], k: int) -> List[int]:
+        resultado = []
+
+        for i in range(0, len(nums), k):
+            bloque = nums[i:i+k]      
+            resultado += bloque[::-1] 
+        return resultado
+
 
 example = Solution()
 print(example.reverseFirstHalf([1, 2, 3, 4, 5, 6]))
@@ -20,3 +28,6 @@ print(example.reverseFirstHalf([1, 2, 3, 4, 5, 6]))
 
 example2 = Solution()
 print(example.changeOrder([1, 2, 3, 4, 5, 6]))
+
+example3 = Solution()
+print(example.reverseKGroup([1, 2, 3, 4, 5, 6], 2))
