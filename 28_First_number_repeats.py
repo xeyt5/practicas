1629d146d27285ed0f63645fc6411f24449d66b7
Xeyt <cbwyuren500@gmail.com>
2026-01-12 13:23:49 -0600

    ejercicio 28 se actualizo junto con el primer numero que no se repite

--- 28_First_number_repeats.py
+++ 28_First_number_repeats.py
@@ -7,5 +7,15 @@ class Soulution:
             seen.add(num)
         return None
     
+    def first_number_not_repeated(self, nums):
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        for num in nums:
+            if count[num] == 1:
+                return num
+        return None
+    
 example = Soulution()
 print(example.first_repeated_number([2, 3, 1, 5, 3, 2]))  
+print(example.first_number_not_repeated([2, 3, 1, 5, 3, 2]))
